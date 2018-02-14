16f88abbbaca6921eb5e44c0e501d6528c86d034
kodycode <kodythach@gmail.com>
2018-02-13 21:21:23 -0500

    Final code

--- cogs/modules/subscriber_functionality.py
+++ cogs/modules/subscriber_functionality.py
@@ -137,12 +137,6 @@ class SubscriberFunctionality:
             if not valid_time:
                 return None
             if channel_settings["currencies"]:
-                if channel_settings["purge"]:
-                    try:
-                        await self.bot.purge_from(channel,
-                                                  limit=10)
-                    except:
-                        pass
                 return self.coin_market.get_current_multiple_currency(self.market_list,
                                                                       None,
                                                                       self.cache_data,
@@ -156,6 +150,7 @@ class SubscriberFunctionality:
         @param minute - the minute the clock is at
         """
         try:
+            logger.info("Displaying live data.")
             self._check_invalid_sub_currencies()
             subscriber_list = self.subscriber_data.copy()
             for channel in subscriber_list:
@@ -167,24 +162,33 @@ class SubscriberFunctionality:
                 data = await self._get_live_data(channel,
                                                  channel_settings,
                                                  minute)
-                return data
+                return channel, data
 
-            async def post_data(data_set):
+            async def post_data(data_set, bot):
                 for data in limited_as_completed(data_set, 10):
                     first_post = True
-                    for msg in await data:
-                        if first_post:
-                            em = discord.Embed(title="Live Currency Update",
-                                               description=msg,
-                                               colour=0xFF9900)
-                            first_post = False
-                        else:
-                            em = discord.Embed(description=msg,
-                                               colour=0xFF9900)
-                        await self._say_msg(channel=self.cache_channel[channel],
-                                            emb=em)
+                    data = await data
+                    ch = data[0]
+                    data = data[1]
+                    channel_settings = subscriber_list[ch]
+                    if data is not None:
+                        if channel_settings["purge"]:
+                            await bot.purge_from(bot.get_channel(ch),
+                                                 limit=10)
+                        for msg in data:
+                            if first_post:
+                                em = discord.Embed(title="Live Currency Update",
+                                                   description=msg,
+                                                   colour=0xFF9900)
+                                first_post = False
+                            else:
+                                em = discord.Embed(description=msg,
+                                                   colour=0xFF9900)
+                            await self._say_msg(channel=self.cache_channel[ch],
+                                                emb=em)
             coros = (get_data(channel, minute) for channel in self.cache_channel)
-            self.bot.loop.create_task(post_data(coros))
+            self.bot.loop.create_task(post_data(coros, self.bot))
+            logger.info("Done.")
         except CurrencyException as e:
             print("An error has occured. See error.log.")
             logger.error("CurrencyException: {}".format(str(e)))
